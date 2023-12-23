4ee58bdc9b4ae5bc830283a7029e60968da31ec6
Jose Maria <chchema26@gmail.com>
2023-12-23 11:21:35 -0600

    Detail, List, Update and Delete pages with views and urls added

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PageListView, PageDetailView
+from .views import PageListView, PageDetailView, PageCreateView, PageUpdateView, PageDelete
 
 
-urlpatterns = [
+pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
-    path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page')
-]
+    path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
+    path('create/', PageCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', PageUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', PageDelete.as_view(), name='delete')
+], 'pages')
--- pages/views.py
+++ pages/views.py
@@ -1,25 +1,38 @@
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
 from .models import Page
 
 # Create your views here.
+class PageCreateView(CreateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
+
+# Update View
+class PageUpdateView(UpdateView):
+    model = Page
+    fields = ['title', 'content','order']
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('pages:pages')
+    
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id] ) + '?ok'
 
 # Detail View
 class PageDetailView(DetailView):
     model = Page
-    
-    
-# def page(request, page_id, page_slug):
-#     page = get_object_or_404(Page, id=page_id)
-#     return render(request, 'pages/page.html', {'page':page})
 
 
 # List View
 class PageListView(ListView):
     model = Page
     
-# def pages(request):
-#     pages = get_list_or_404(Page)
-#     return render(request, 'pages/pages.html', {'pages':pages})
+
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
+    
+    
 
