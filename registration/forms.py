10834636bda5b40912d7c2f5d6d4fd39e20ae7ca
Jose Maria <chchema26@gmail.com>
2024-04-10 11:47:45 -0600

    Email Validation

--- registration/forms.py
+++ registration/forms.py
@@ -8,3 +8,12 @@ class UserCreationFormWithEmail(UserCreationForm):
     class Meta:
         model = User
         fields = ("username","email","password1", "password2")
+        
+        
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('El email ya esta registrado, usa otro email.')
+        
+            
+        return email
